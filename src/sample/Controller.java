f5d138ba0d01a1d276e2bf7f91ef0003170f3358
nocaed <codenamedeacon@outlook.com>
2020-03-16 17:30:05 -0400

    Added all GUI objects to Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.CheckBox;
+
 public class Controller {
+    @FXML
+    private URL location;
+    @FXML
+    private ResourceBundle resources;
+    @FXML
+    private TextField fnameTxt,
+                      lnameTxt,
+                      creditsTxt,
+                      valTxt;
+    @FXML
+    private Button addBtn,
+                   removeBtn,
+                   printBtn;
+    @FXML
+    private RadioButton inRad,
+                        outRad,
+                        intRad;
+    @FXML
+    private CheckBox findingChk,
+                     triChk,
+                     exchangeChk;
+    @FXML
+    private TextArea outputTextArea;
 }
