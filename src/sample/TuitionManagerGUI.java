5ad3f5ec40be0823e653ad0f0d1ec295710a6756
nocaed <codenamedeacon@outlook.com>
2020-03-18 12:05:35 -0400

    refactored file name

--- src/sample/TuitionManagerGUI.java
+++ src/sample/TuitionManagerGUI.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class TuitionManager extends Application {
+public class TuitionManagerGUI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
